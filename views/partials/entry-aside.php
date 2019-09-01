1fa356a777c3296ef32d4bb32afb24413e92291a
Paulo Gomes <paulo@appnovation.com>
2019-09-02 00:18:15 +0100

    improving change of entry moderation status

--- views/partials/entry-aside.php
+++ views/partials/entry-aside.php
@@ -1,12 +1,14 @@
 <div class="uk-margin moderation-status" if="{field && field.length && moderation_field}">
   <div class="uk-width-1-1 uk-form-select uk-moderation-element uk-moderation-{ entry[moderation_field] }">
-    <label class="uk-text-small">@lang('Moderation Status:')</label>
+    <label class="uk-text">
+      <i if="{originalModeration == 'Unpublished'}" class="icon-Unpublished uk-icon-circle-o"></i>
+      <i if="{originalModeration == 'Draft'}" class="icon-Draft uk-icon-pencil"></i>
+      <i if="{originalModeration == 'Published'}" class="icon-Published uk-icon-circle"></i>
+      <strong>@lang('Status:')</strong> {originalModeration}
+    </label>
     <div class="uk-margin-small-top">
       <span class="uk-badge uk-badge-outline">
-        <i if="{entry[moderation_field] == 'Unpublished'}" class="uk-icon-circle-o"></i>
-        <i if="{entry[moderation_field] == 'Draft'}" class="uk-icon-pencil"></i>
-        <i if="{entry[moderation_field] == 'Published'}" class="uk-icon-circle"></i>
-        @lang("{entry[moderation_field]}")
+        @lang("Change to:") <strong>@lang("{entry[moderation_field]}")</strong>
       </span>
     </div>
     <select bind="entry.{moderation_field}">
@@ -20,6 +22,7 @@
 <script>
   var $this = this;
   $this.moderation_field = 'status';
+  $this.originalModeration = '';
   $this.canPublish = {{ $app->module("cockpit")->hasaccess('moderation', ['manage', 'publish']) ? 1 : 0 }};
   $this.canUnpublish = {{ $app->module("cockpit")->hasaccess('moderation', ['manage', 'unpublish']) ? 1 : 0 }};
 
@@ -35,10 +38,8 @@
 
     $this.moderation_field = $this.field[0].name;
 
-    $this.entry[$this.moderation_field] = $this.entry[$this.moderation_field] || 'Draft';
-    if (!$this.canPublish && $this.entry[$this.moderation_field] === 'Published') {
-      $this.entry[$this.moderation_field] = 'Draft';
-    }
+    $this.originalModeration = $this.entry[$this.moderation_field] || 'Draft';
+    $this.entry[$this.moderation_field] = 'Draft';
 
     window.setTimeout(function() {
       sidebar = document.querySelector('.uk-width-medium-1-4.uk-flex-order-first');
