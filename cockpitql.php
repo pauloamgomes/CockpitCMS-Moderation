fee269acf345335d5de23f7209e39398a72e7e1e
Paulo Gomes <paulo@appnovation.com>
2019-10-18 19:53:06 +0100

    handle of rescheduling

--- cockpitql.php
+++ cockpitql.php
@@ -5,3 +5,8 @@ use GraphQL\Type\Definition\Type;
 $app->on('cockpitql.type.moderation', function ($field, &$def) use ($app) {
   $def['type'] = Type::string();
 });
+
+// API includes.
+if (COCKPIT_API_REQUEST) {
+  include_once __DIR__ . '/cockpitql.php';
+}
--- views/partials/entry-aside.php
+++ views/partials/entry-aside.php
@@ -1,4 +1,58 @@
-<div class="uk-margin moderation-status" if="{field && field.length && moderation_field}">
+<div ref="scheduleModal" class="uk-modal uk-modal-schedule">
+  <div class="uk-modal-dialog uk-form">
+      <h3 class="uk-text-bold uk-flex uk-flex-middle">@lang('Set Schedule')</h3>
+      <div class="uk-margin-top uk-overflow-container uk-panel1">
+          <div class="uk-text-muted">
+            <span>@lang('Configure the scheduling moderation status and the date/time.')</span>
+          </div>
+          <div class="schedule-container">
+              <div class="uk-margin-top" if="{ lang }">
+                  <div class="uk-flex-item-1">
+                    <label class="uk-text-small">@lang('Language:')</label><br />
+                    <span>{ getLangLabel(lang) }</span>
+                  </div>
+              </div>
+              <div class="uk-margin-top">
+                  <div class="uk-flex-item-1">
+                    <div class="uk-form-select" data-uk-form-select>
+                        <label class="uk-text-small">@lang('Moderation Status:')</label>
+                        <input placeholder="select..." class="uk-width-1-1 uk-form-blank" value="{ schedule && schedule.type }">
+                        <select bind="schedule.type">
+                          <option value="">Select</option>
+                          <option if="{ canPublish }" value="Publish">@lang("Publish")</option>
+                          <option if="{ canUnpublish }" value="Unpublish">@lang("Unpublish")</option>
+                        </select>
+                      </div>
+                  </div>
+              </div>
+              <div class="uk-margin-top uk-grid">
+                  <div class="uk-width-1-2">
+                    <label class="uk-text-small">@lang('Select a future Date:')</label>
+                    <field-date bind="schedule.date" placeholder="select date..."></field-date>
+                  </div>
+                  <div class="uk-width-1-2 schedule-time">
+                    <label class="uk-text-small">@lang('Select Time:')</label><br />
+                    <field-time bind="schedule.time"></field-time>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="uk-modal-footer uk-text-right">
+          <button if="{ scheduleIsValid() }" class="uk-button uk-button-link uk-button-large" onclick="{ setSchedule }">
+            @lang('Save')
+          </button>
+          <button if="{ !scheduleIsValid() }" class="uk-button uk-button-link uk-button-large" disabled>
+            @lang('Save')
+          </button>
+          <button class="uk-button uk-button-link uk-button-large uk-modal-close" onclick="{ closeSchedule }">
+            @lang('Cancel')
+          </button>
+      </div>
+  </div>
+</div>
+
+<div class="uk-margin moderation-status { canSchedule ? 'with-schedule' : '' }" if="{field && field.length && moderation_field}">
+  <label class="uk-text-small">@lang('Moderation')</label>
   <div class="uk-width-1-1 uk-form-select uk-moderation-element uk-moderation-{ entry[moderation_field] }">
     <label class="uk-text">
       <i if="{originalModeration == 'Unpublished'}" class="icon-Unpublished uk-icon-circle-o"></i>
@@ -17,14 +71,28 @@
       <option if="{ canPublish }" selected="{ entry[moderation_field] === 'Published' }" value="Published">@lang("Published")</option>
     </select>
   </div>
+  <div if="{ canSchedule && entry._id }" class="uk-margin-small-top">
+    <label if="{ scheduleIsValid() }" class="uk-text-small">@lang('Schedule')</label>
+    <div if="{ !scheduleIsValid() }" class="uk-text-small uk-text-muted">
+      <a class="uk-text-medium" onClick="{ showSchedule }"><i class="uk-icon-clock-o"></i> @lang('Schedule')</a>
+    </div>
+    <div if="{ scheduleIsValid() }" class="uk-text-medium uk-text-muted">
+      <strong><i class="uk-icon-clock-o"></i> { schedule.type }</strong><br />
+      <span>{ schedule.date } { schedule.time }</span> <a class="uk-text-small uk-text-danger" onClick="{ cancelSchedulePrompt }"><i class="uk-icon-trash"></i> @lang('Cancel')</a>
+    </div>
+  </div>
 </div>
 
 <script>
   var $this = this;
+  $this.scheduleModal = null;
   $this.moderation_field = 'status';
   $this.originalModeration = '';
   $this.canPublish = {{ $app->module("cockpit")->hasaccess('moderation', ['manage', 'publish']) ? 1 : 0 }};
   $this.canUnpublish = {{ $app->module("cockpit")->hasaccess('moderation', ['manage', 'unpublish']) ? 1 : 0 }};
+  $this.canSchedule = {{ json_encode($enabled) }};
+  $this.schedule = false;
+  $this.langLabel = null;
 
   var oldXHROpen = window.XMLHttpRequest.prototype.open;
   window.XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
@@ -47,7 +115,6 @@
     }
 
     $this.moderation_field = $this.field[0].name;
-
     $this.originalModeration = $this.entry[$this.moderation_field] || 'Draft';
     $this.entry[$this.moderation_field] = 'Draft';
 
@@ -56,8 +123,98 @@
       sidebar.insertBefore(document.querySelector('.moderation-status'), sidebar.childNodes[0]);
     }, 50);
 
+    if (this.canSchedule && this.entry._id) {
+      this.getSchedule();
+    }
+
+    this.scheduleModal = UIkit.modal(this.refs.scheduleModal, { modal: false, 'escclose': false, 'bgclose': false, 'keyboard': false });
+    this.scheduleModal.on('hide.uk.modal', this.hideSchedule);
+
     $this.update();
   });
+
+  this.on('bindingupdated', function(data) {
+    if (this.entry._id && this.canSchedule && data[0] && data[0] === 'lang') {
+      this.getSchedule();
+    }
+  });
+
+  this.getSchedule = function() {
+    var filter = {
+      id: this.entry._id,
+      lang: this.lang || ""
+    };
+    App.callmodule('moderation:getSchedule', filter, 'schedule').then(function(data) {
+      $this.schedule = (data.result && data.result.schedule) || null;
+      $this.update();
+    }).catch(function(e){
+      App.ui.notify('Error loading schedule information!', 'danger');
+    });
+  }
+
+  this.setSchedule = function() {
+    if (!this.scheduleDateIsValid()) {
+      App.ui.notify('Invalid schedule date/time, must be a valid future date', 'danger');
+      return;
+    }
+    var schedule = {
+      id: this.entry._id,
+      schedule: this.schedule,
+      lang: this.lang || ""
+    };
+    App.callmodule('moderation:setSchedule',  schedule, 'schedule').then(function(data) {
+      App.ui.notify('Schedule: entry will be <strong>' + $this.schedule.type + "</strong> on <strong>" + $this.schedule.date + " " + $this.schedule.time + '</strong>', 'success');
+    }).catch(function(e){
+      App.ui.notify('Error creating schedule!', 'danger');
+    });
+    $this.scheduleModal.hide();
+    $this.update();
+  }
+
+  this.hideSchedule = function(e) {
+    if (!$this.scheduleIsValid()) {
+      $this.schedule = null;
+      $this.update();
+    }
+  }
+
+  this.closeSchedule = function(e) {
+    $this.schedule = null;
+    $this.update();
+  }
+
+  this.showSchedule = function() {
+    $this.scheduleModal.show();
+  }
+
+  this.cancelSchedulePrompt = function() {
+    App.ui.confirm("Are you sure?", function() {
+      App.callmodule('moderation:removeSchedule', { id: $this.entry._id, lang: $this.lang || "" }, 'schedule').then(function(data) {
+        $this.schedule = null;
+        App.ui.notify('Schedule removed!', 'success');
+        $this.update();
+      }).catch(function(e) {
+        App.ui.notify('Error removing schedule!', 'danger');
+      });
+    });
+  }
+
+  this.scheduleIsValid = function() {
+    return this.schedule && this.schedule.type && $this.schedule.date && $this.schedule.time;
+  }
+
+  this.scheduleDateIsValid = function() {
+    var sdate = moment($this.schedule.date + " " + $this.schedule.time);
+    return sdate.isAfter(moment(), 'hour');
+  }
+
+  this.getLangLabel = function(code) {
+    for (key in this.languages) {
+      if (this.languages[key].code === code) {
+        return App.i18n.get(this.languages[key].label);
+      }
+    }
+  }
 
 
 </script>
